0e5b9d080fcc0d8df41f6a011baa735460d4f4f3
Carlos Zurita <carlos.zurita3432@gmail.com>
2025-08-09 00:29:18 -0400

    implemantado funciones

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+const agregarlistaAmigo = document.getElementById('amigo');
+const listaAmigos = [];
+const mostrarAmigos = document.getElementById('listaAmigos');
+const resultadoAmigo = document.getElementById('resultado');
+
+
+
+
+function agregarAmigo(){
+    const nombre = agregarlistaAmigo.value.trim();
+
+    if(validarNombre(nombre)){
+        listaAmigos.push(nombre);
+        mostrarAmigos.innerHTML += `<li>${nombre}</li>`;
+        limpiarCaja();
+    }
+
+    //console.log(listaAmigos);
+}
+
+function validarNombre(nombre){
+    const soloLetras = /^[A-Za-z\s]+$/;
+    if (!nombre) {
+        alert('Por favor ingrese un nombre');
+        return false;
+    } else if (!soloLetras.test(nombre)) {
+        alert('El nombre solo puede contener letras y espacios. No se permiten números ni símbolos.');
+        limpiarCaja();
+        return false;
+    }
+    return true;
+}
+
+function sortearAmigo(){
+
+    if(listaAmigos.length == ""){
+        alert('por favor ingrese al menos un nombre');
+        return;
+    }
+    const numAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    //console.log(numAleatorio);
+    const amigoSecreto = listaAmigos[numAleatorio];
+    resultadoAmigo.innerHTML = `El amigo secreto es: <li>${amigoSecreto}</li>`;
+    limpiarListaAmigos();
+}
+
+
+
+function limpiarCaja (){
+    document.querySelector('#amigo').value = "";
+}
+
+
+function limpiarListaAmigos(){
+   
+    mostrarAmigos.innerHTML = "";
+    //console.log("se limpio lista");
+}
+
